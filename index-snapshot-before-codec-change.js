a80a5a9f5b08ff167fd40307c0a3a43d447cce69
ainsleys <sutherlande@gmail.com>
2016-10-05 15:03:14 -0400

    initial commit

---
+++ index-snapshot-before-codec-change.js
@@ -0,0 +1,151 @@
+var express = require('express');
+var app = express();
+var mongo = require('mongodb').MongoClient;
+
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+
+//Lets load the mongoose module in our program
+var mongoose = require('mongoose');
+
+//Lets connect to our database using the DB server URL.
+mongoose.connect('mongodb://localhost/my_database_name');
+
+var schema = new mongoose.Schema({id: String, position_x: Number, position_y: Number, position_z: Number, audioObject: Object});
+
+var audiomessage = mongoose.model('audiomessage', schema);
+
+
+
+
+
+var path = require('path');
+var wav = require('wav');
+var ogg = require('ogg');
+var stringify = require('node-stringify');
+//var multer  = require('multer')
+var three = require('three');
+var webvrboilerplate = require('webvr-boilerplate');
+
+
+
+var counter = 0;
+var streamcounter = 0;
+var streamOutfile = 'streamdemo.wav';
+
+var opus = require('node-opus');
+var oggtrack = '/public/recordings/track.ogg';
+var fileName = new Date().toISOString() + ".ogg";
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/public/testpractice.ogg', function (req, res) {
+  res.sendFile(__dirname + '/public/testpractice.ogg');
+});
+
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/my_database_name';
+
+//app.post('/audio/upload', upload.array('audio', )
+var practice = './public/testpractice.ogg'; //this should be the 
+
+//maybe there should be no io.on? Just socket on?
+io.on('connection', function(socket){
+
+
+  	console.log('a user connected');
+  	
+  	
+
+
+
+  	socket.on('audioObjectReceiver', function(){
+		//finds EVERY audio object
+			audiomessage.find({ }, function (err, userObj) {
+		  	if (err) {
+		    	console.log(err);
+		  	} else if (userObj) {
+		  		var res = userObj;
+		    	console.log('Found:', res);
+		    	io.emit('audioObjectReceiver', res)
+		    	}
+
+		 //for each object in res, populate the x/y/z/, put a sphere, decode the mp3
+		 //new files shoudl be emitted to all users when they're made, as well as pushed to the initial check
+
+
+		  	console.log(res);
+		  	console.log(res.length);
+		  	console.log(res.typeOf);
+			});
+
+
+  		//fs.writeFile(practice, dataBlob, 'base64', function (err) {
+  		//	if (err) return console.log(err);
+  		//	console.log('dataBlob > practice');
+		//});
+
+  		//can we emit the file back lmao?
+		//io.emit('existingAudio', practice);
+
+  	});
+
+  	socket.on('positionTest', function(dataBlob){
+  		console.log(Buffer.isBuffer(dataBlob['audioMessage']));
+  		console.log(typeof(dataBlob['audioMessage']));
+  		var audioMessage1 = new audiomessage(dataBlob);
+  		//THIS isnt working!
+  		console.log(dataBlob);
+  		console.log(audioMessage1);
+
+			audioMessage1.save(function (err, userObj) {
+			  			if (err) {
+			    			console.log(err);
+			  			} else {
+			    			console.log('saved successfully:', dataBlob['id']);
+			  			}
+					});
+  		//var test = stringify(dataBlob);
+  		//can we emit the file back lmao?
+  	});
+
+ //  	socket.on('databaseTest', function(existingAudio){
+ //  		  	mongo.connect(url, function(err, db){
+ //  			var collection = db.collection('audioMessages');
+ //  			var result = collection.find({});
+ //  		})
+
+	// 	io.emit('existingAudio', result);
+	// });
+
+
+  	//server receives message from client
+  	socket.on('chat message', function(msg){
+    	console.log('message: ' + msg);
+    //emits message back to ALL clients including sender
+    	io.emit('chat message', msg);
+
+  	});
+  
+
+	socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
